1617098aa606c1e0cbdb3a3535567dc2c3762bae
Om Sharma <omsharma050322@gmail.com>
2022-10-14 23:36:08 +0530

    Added HeroSection

--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -8,21 +8,27 @@ function Herosection() {
             <div className="container">
                 <div className="box">
                     <div className="hero-content">
-                        <h1 style={{ paddingTop: '5rem', margin: 'auto 0', fontWeight: 'normal' }}>
-                            Complete guide for CBSE{' '}
-                            <span style={{ color: 'red', fontWeight: 'bold' }}>
-                                <Typewriter
-                                    words={['Students.', 'Teachers.', 'Parents.']}
-                                    loop={5}
-                                    cursor
-                                    cursorStyle='|'
-                                    typeSpeed={130}
-                                    deleteSpeed={60}
-                                    delaySpeed={1000}
-                                />
-                            </span>
-                        </h1>
-                        <p>Learning App for CBSE students and Test Generator for teachers to create question papers in minutes.</p>
+                        <div className='sec1'>
+                            <h1>
+                                Complete Guide For CBSE{' '}
+                                <span style={{ color: 'red', fontWeight: 'bold' }}>
+                                    <Typewriter
+                                        words={['Students.', 'Teachers.', 'Parents.']}
+                                        loop={5}
+                                        cursor
+                                        cursorStyle='|'
+                                        typeSpeed={130}
+                                        deleteSpeed={60}
+                                        delaySpeed={1000}
+                                    />
+                                </span>
+                            </h1>
+                            <p>Learning App for CBSE students and Test Generator for teachers to create question papers in minutes.</p>
+                            <a href="#" className='classbutton'>Classes</a>
+                        </div>
+                        <div className="sec2">
+                            <img src="https://media-mycbseguide.s3.amazonaws.com/images/direct/home_top.png" alt="image" />
+                        </div>
 
                     </div>
                 </div>
